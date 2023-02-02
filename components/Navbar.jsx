13aa946c1e488566817f57ca5a495b633671e135
minha0817 <minhakk24@gmail.com>
2023-02-02 12:37:54 -0800

    Add Links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import react from 'react'
 export default function Navbar() {
   return (
     <nav>
-      <h1>Hipapa</h1>
+      <Link href="/"><h1>Hipapa</h1></Link>
       <p>Welcome, teacher!</p>
       <p>logout</p>
       <Link href="/reports">reports</Link>
--- pages/reports/index.js
+++ pages/reports/index.js
@@ -1,7 +1,16 @@
-import react from "react";
+import Link from "next/link";
+import React from "react";
 
-export default function Reports(){
+export default function Reports() {
   return (
-    <h1>reports</h1>
-  )
+    <>
+      <h1>reports</h1>
+      <article>
+        <Link href="/reports/incidents"><h2>Incident</h2></Link>
+      </article>
+      <article>
+        <h2>Meals</h2>
+      </article>
+    </>
+  );
 }
